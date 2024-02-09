a51b0be633983914a9e15467c077c5a0ea8cb452
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-09 17:24:30 +0100

    Refactor agencyController.ts

--- back-end/src/controllers/agencyController.ts
+++ back-end/src/controllers/agencyController.ts
@@ -2,17 +2,8 @@ import { Request, Response, json } from "express";
 import Agency, { IAgency } from "../models/Agency";
 import User,{ IUser } from "../models/User";
 
-const Joi = require("joi");
 
-const registrationSchema = Joi.object({
-    phone: Joi.number().required(),
-    email: Joi.string().required(),
-    password: Joi.string().required(),
-    role: Joi.string().required(),
-  agencyName: Joi.string().required(),
-  address: Joi.string().required(),
-  availableCars: Joi.number().required(),
-});
+
 
 export const agencyController = {
   getAllAgencys: async (req: Request, res: Response) => {
@@ -34,68 +25,7 @@ export const agencyController = {
     }
   },
 
-  createAgency: async (req: Request, res: Response) => {
-    try {
-      const { error } = registrationSchema.validate(req.body);
-      if (error) {
-        return res.status(400).json({ error: error.details[0].message });
-      }
-
-      const {
-        phone,
-        email,
-        password,
-        role,
-        agencyName,
-        address,
-        availableCars,
-      } = req.body;
-
-      if (
-        !(
-          agencyName &&
-          address &&
-          availableCars &&
-          phone &&
-          email &&
-          password &&
-          role
-        )
-      ) {
-        return res
-          .status(400)
-          .json({ error: "Please provide all required fields" });
-      }
-
-      const newUser: IUser = new User({
-        phone,
-        email,
-        password,
-        role
-      });
-      const savedUser = await newUser.save();
-
-      const newAgency: IAgency = new Agency({
-        agencyName,
-        address,
-        availableCars,
-        idUser: savedUser._id,
-      });
-
-
-    const savedAgency = await newAgency.save();
-    savedUser.idUser = savedAgency._id;
-    await savedUser.save();
-
-      res.status(201).json({
-        message: "Agency created successfully",
-        agency: savedAgency,
-        user: savedUser,
-      });
-    } catch (error) {
-      console.error(error);
-    }
-  },
+  
 
   updateAgency: async (req: Request, res: Response) => {
     const { id } = req.params;
