a2a3ab5a655f741b6bcc62ed3c9d83624b8b2d7a
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-11 17:16:09 +0100

    Refactor JWT authentication middleware

--- back-end/src/middleware/jwt.ts
+++ back-end/src/middleware/jwt.ts
@@ -3,41 +3,33 @@ import dotenv from "dotenv";
 import { NextFunction, Request, Response } from "express";
 dotenv.config();
 
-  
 interface UserRequest extends Request {
-    user: {
-        userId: string;
-        fullName: string;
-        email: string;
-        role: string;
-    };
+  user: {
+    userId: string;
+  };
 }
 
-
-export const authenticateToken = (req:Request, res:Response, next:NextFunction) => {
+export const authenticateToken = (
+  req: Request,
+  res: Response,
+  next: NextFunction
+) => {
   const tokenWithBearer = req.headers.authorization;
-  if (!tokenWithBearer)
-    return res.status(401).json({ error: "Access denied" });
+  if (!tokenWithBearer) return res.status(401).json({ error: "Access denied" });
   const [, token] = tokenWithBearer.split(" ");
- 
-try {
+
+  try {
     if (token) {
-            console.log(process.env.TOKEN_SECRET);
-            
-        const decoded = jwt.verify(token, process.env.TOKEN_SECRET!);
-        
-        if (decoded && typeof decoded === "object") {
-            const { sub, fullName, email, role } = decoded;
-            (req as UserRequest).user = {
-                userId: sub!,
-                fullName: fullName,
-                email: email,
-                role: role,
-            };
-        }
+      const decoded = jwt.verify(token, process.env.TOKEN_SECRET!);
+      if (decoded && typeof decoded === "object") {
+        const { id} = decoded;
+        (req as UserRequest).user = {
+          userId: id
+        };
+      }
     }
-    next();
-} catch (error) {
+    return next();
+  } catch (error) {
     res.status(400).json({ error: "Invalid token" });
   }
 };
