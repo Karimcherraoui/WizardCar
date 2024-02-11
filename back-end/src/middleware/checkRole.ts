2f80c01e5a87a133a37c4bdcc32d7042b85d5d30
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-11 17:16:15 +0100

    Add checkRole middleware for client and agency authentication

---
+++ back-end/src/middleware/checkRole.ts
@@ -0,0 +1,55 @@
+import { NextFunction, Request, Response } from "express";
+import User from "../models/User";
+
+interface UserRequest extends Request {
+    user: {
+      userId: string;
+    };
+  }
+export const checkRole ={
+
+client: async  (
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+        console.log("Client authentication");
+        try {
+            const userId =(req as UserRequest).user.userId;
+            const user = await User.findById(userId );
+            const role = user?.role;
+            if (role === "client" || role === "admin") {
+                next();
+            } else {
+                res.status(401).json({ error: "Access denied" });
+            }
+        } catch (error) {
+            console.error("Error in Client authentication ", error);
+            res.status(500).json({ error: "Internal Server Error" });    
+
+        }
+},
+
+agency: async  (
+    req: Request,
+    res: Response,
+    next: NextFunction
+    ) => {
+            console.log("Agency authentication");
+            
+        try {
+            const userId =(req as UserRequest).user.userId;
+            const user = await User.findById(userId );
+            const role = user?.role;
+            if (role === "agency" || role === "admin") {
+                next();
+            } else {
+                res.status(401).json({ error: "Access denied" });
+            }
+        } catch (error) {
+            console.error("Error in Agency authentication ", error);
+            res.status(500).json({ error: "Internal Server Error" });    
+        }
+
+}
+}
