0961df7a540182c5a5483865df6603eafb361d7d
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-13 10:50:22 +0100

    Update carRoute.ts with new routes

--- back-end/src/routes/carRoute.ts
+++ back-end/src/routes/carRoute.ts
@@ -1,23 +1,16 @@
-
-import { Router} from "express";
+import { Router } from "express";
 import { carController } from "../controllers/carConroller";
 import { authenticateToken } from "../middleware/jwt";
 import { checkRole } from "../middleware/checkRole";
-
-
-
-
 
 const routerCar = Router();
 
 routerCar.get("/", carController.getAllCars);
 routerCar.get("/:id", carController.getCar);
 routerCar.use(authenticateToken);
-routerCar.patch("/:id", checkRole.agency ,carController.updateCar);
-routerCar.delete("/:id", checkRole.agency ,carController.deleteCar);
-routerCar.post("/", checkRole.agency ,carController.createCar);
-
-
-
+routerCar.patch("/:id", checkRole.agency, carController.updateCar);
+routerCar.patch("/returnCar/:id", checkRole.agency, carController.returnCar);
+routerCar.delete("/:id", checkRole.agency, carController.deleteCar);
+routerCar.post("/", checkRole.agency, carController.createCar);
 
 export default routerCar;
