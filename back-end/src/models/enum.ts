f9cfe3b7a04a62966d82cafe44554c7bfd57af06
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-15 00:24:44 +0100

    Add enum constants for reservation, role, car type, fuel, transmission, and availability

---
+++ back-end/src/models/enum.ts
@@ -0,0 +1,34 @@
+export const ReservationEnum = {
+    ATTENTE: 'En attente',
+    CONFIRME: 'Confirmée',
+    ANNULEE: 'Annulée'
+  };
+
+  
+    export const RoleEnum = {
+        ADMIN: 'admin',
+        CLIENT: 'client',
+        AGENCY: 'agency'
+    };
+
+    export const CarTypeEnum = {
+        CITADINE: 'Citadine',
+        BERLINE: 'Berline',
+        SUV: 'SUV',
+        UTILITAIRE: 'Utilitaire'
+    };
+
+    export const FuelEnum = {
+        DIESEL: 'Diesel',
+        ESSENCE: 'Essence'
+    };
+
+    export const TransmissionEnum = {
+        MANUELLE: 'Manuelle',
+        AUTOMATIQUE: 'Automatique'
+    };
+
+    export const disponibilityEnum = {
+        DISPONIBLE: 'Disponible',
+        NON_DISPONIBLE: 'Non disponible'
+    };
