41ce07e86e3a8736093de0166f1ed0db9d74345f
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-17 20:25:16 +0100

    Refactor Blog page layout

--- frontend/src/pages/Blog.jsx
+++ frontend/src/pages/Blog.jsx
@@ -1,5 +1,4 @@
 import React from "react";
-import { Container } from "reactstrap";
 import Helmet from "../components/Helmet/Helmet";
 import CommonSection from "../components/UI/CommonSection";
 import BlogList from "../components/UI/BlogList";
@@ -8,11 +7,8 @@ const Blog = () => {
   return (
     <Helmet title="Blogs">
       <CommonSection title="Blogs" />
-      <section className="container mx-auto">
-        <div className="grid grid-cols-1 md:grid-cols-2 gap-4">
-          <BlogList />
-          <BlogList />
-        </div>
+      <section className="container flex flex-col  md:flex-wrap justify-between mx-auto md:mr-10 my-20 gap-4">
+        <BlogList />
       </section>
     </Helmet>
   );
