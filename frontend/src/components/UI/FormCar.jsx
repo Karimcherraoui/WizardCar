d44983ea22283bd54b94660e8f09f67eb137a3b5
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-17 20:24:33 +0100

    Add FormCar component

---
+++ frontend/src/components/UI/FormCar.jsx
@@ -0,0 +1,158 @@
+import React from "react";
+
+export default function FormCar() {
+  return (
+    <>
+      <section className="max-w-4xl p-6 mx-auto bg-indigo-600 rounded-md shadow-md dark:bg-gray-800 mt-10">
+        <h1 className="text-xl font-bold text-white capitalize dark:text-white">
+          Add new car
+        </h1>
+        <form>
+          <div className="grid grid-cols-1 gap-6 mt-4 sm:grid-cols-2">
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="brand">
+                Brand Name
+              </label>
+              <input
+                id="brand"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="model">
+                Model
+              </label>
+              <input
+                id="model"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="year">
+                Year
+              </label>
+              <input
+                id="year"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="price">
+                Price
+              </label>
+              <input
+                id="price"
+                type="price"
+                className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="color">
+                Color
+              </label>
+              <input
+                id="color"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              />
+            </div>
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="fuel">
+                Fuel
+              </label>
+              <select className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                <option>Essence</option>
+                <option>Diesel</option>
+                <option>Electric</option>
+              </select>
+            </div>
+            <div>
+              <label className="text-white dark:text-gray-200" htmlFor="type">
+                Type
+              </label>
+              <select className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                <option>Citadine</option>
+                <option>Berline</option>
+                <option>SUV</option>
+                <option>Utilitaire</option>
+              </select>
+            </div>
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="transmission"
+              >
+                Transmission
+              </label>
+              <select className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring">
+                <option>Manuelle</option>
+                <option>Automatique</option>
+              </select>
+            </div>
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="plateNumber"
+              >
+                Plate Number
+              </label>
+              <input
+                id="plateNumber"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              ></input>
+            </div>
+
+            <div>
+              <label
+                className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                htmlFor="file_input"
+              >
+                Upload image
+              </label>
+              <input
+                className="block w-full text-sm text-gray-900 border border-gray-300 rounded-lg cursor-pointer bg-gray-50 dark:text-gray-400 focus:outline-none dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400"
+                aria-describedby="file_input_help"
+                id="file_input"
+                type="file"
+              />
+              <p
+                className="mt-1 text-sm text-gray-500 dark:text-gray-300"
+                id="file_input_help"
+              >
+                SVG, PNG, JPG or GIF (MAX. 800x400px).
+              </p>
+            </div>
+
+            <div>
+              <label
+                className="text-white dark:text-gray-200"
+                htmlFor="description"
+              >
+                Description
+              </label>
+              <textarea
+                id="description"
+                type="text"
+                className="block w-full px-4 py-2 mt-2 text-black bg-white border border-gray-300 rounded-md dark:bg-gray-800 dark:text-gray-300 dark:border-gray-600 focus:border-blue-500 dark:focus:border-blue-500 focus:outline-none focus:ring"
+              ></textarea>
+            </div>
+      
+          </div>
+
+          <div className="flex justify-end mt-6">
+            <button className="px-6 py-2 leading-5 text-white transition-colors duration-200 transform bg-green-500 rounded-md hover:bg-pink-700 focus:outline-none focus:bg-gray-600">
+              Save
+            </button>
+          </div>
+        </form>
+      </section>
+    </>
+  );
+}
