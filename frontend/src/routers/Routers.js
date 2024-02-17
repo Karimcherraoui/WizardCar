2ed16acdd6ee0df70de89c8703abf03f55adf792
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-02-17 20:25:54 +0100

    Add Login, Register, and DashboardAgency pages to Routers.js

--- frontend/src/routers/Routers.js
+++ frontend/src/routers/Routers.js
@@ -8,18 +8,72 @@ import Blog from "../pages/Blog";
 import BlogDetails from "../pages/BlogDetails";
 import NotFound from "../pages/NotFound";
 import Contact from "../pages/Contact";
-
+import Login from "../pages/Login";
+import Header from "../components/Header/Header";
+import Footer from "../components/Footer/Footer";
+import Register from "../pages/Register";
+import DashboardAgency from "../pages/DashboardAgency";
 const Routers = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Home />} />
-      <Route path="/about" element={<About />} />
-      <Route path="/cars" element={<CarListing />} />
+      <Route
+        path="/home"
+        element={
+          <>
+            <Header />
+            <Home />
+            <Footer />
+          </>
+        }
+      />
+
+<Route path="/" element={<Navigate to="/home" />} />
+
+      <Route
+        path="/about"
+        element={
+          <>
+            <Header />
+            <About />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/cars"
+        element={
+          <>
+            <Header />
+            <CarListing />
+            <Footer />
+          </>
+        }
+      />
       <Route path="/cars/:slug" element={<CarDetails />} />
-      <Route path="/blogs" element={<Blog />} />
+      <Route
+        path="/blogs"
+        element={
+          <>
+            <Header />
+            <Blog />
+            <Footer />
+          </>
+        }
+      />
       <Route path="/blogs/:slug" element={<BlogDetails />} />
-      <Route path="/contact" element={<Contact />} />
+      <Route
+        path="/contact"
+        element={
+          <>
+            <Header />
+            <Contact />
+            <Footer />
+          </>
+        }
+      />
+      <Route path="/login" element={<Login />} />
+      <Route path="/register" element={<Register />} />
+      <Route path="/dashboard" element={<DashboardAgency />} />
       <Route path="*" element={<NotFound />} />
     </Routes>
   );
