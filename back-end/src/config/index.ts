108ed26cfebeb82c22e300b0aaf387679e214dfb
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-18 22:56:13 +0000

    Refactor MongoDB connection in config file

--- back-end/src/config/index.ts
+++ back-end/src/config/index.ts
@@ -1,35 +1,17 @@
-// import mongoose from "mongoose";
-// import "dotenv/config"
+import mongoose from "mongoose";
+import "dotenv/config"
 
-// const db = mongoose
-// const link = process.env.CONNECTION_LINK!
+const db = mongoose
+const link = process.env.CONNECTION_LINK!
 
-//   mongoose.connect(link)
-//   .then(() => {
-//     console.log('Connected to MongoDB');
-//   })
-//   .catch((error) => {
-//     console.error('Error connecting to MongoDB:', error);
-//   });
-
-// export default db;
-
-
-
-import { PrismaClient } from '@prisma/client'
-
-const prisma = new PrismaClient()
-
-async function main() {
-  // ... you will write your Prisma Client queries here
-}
-
-main()
-  .then(async () => {
-    await prisma.$disconnect()
+  mongoose.connect(link)
+  .then(() => {
+    console.log('Connected to MongoDB');
   })
-  .catch(async (e) => {
-    console.error(e)
-    await prisma.$disconnect()
-    process.exit(1)
-  })
+  .catch((error) => {
+    console.error('Error connecting to MongoDB:', error);
+  });
+
+export default db;
+
+
