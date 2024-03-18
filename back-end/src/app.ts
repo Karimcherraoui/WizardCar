db2c6cdd39cfd400767c202e6d3be1206c07f1c5
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-18 22:56:51 +0000

    Update CORS origin to localhost:3000

--- back-end/src/app.ts
+++ back-end/src/app.ts
@@ -7,7 +7,7 @@ const app = express();
 const cors = require('cors');
 
 app.use(cors({
-  origin: 'http://localhost:5173',
+  origin: 'http://localhost:3000',
 }));
 app.use(express.json())
 app.use("/", router)
