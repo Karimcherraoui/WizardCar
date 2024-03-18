e035bf844db73a1def896cf13c24001e80ef9b2e
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-18 22:56:43 +0000

    Add owner details, expiration date, and banking information to Agency model

--- back-end/src/models/Agency.ts
+++ back-end/src/models/Agency.ts
@@ -2,28 +2,45 @@ import mongoose, { Document } from "mongoose";
 
 export interface IAgency extends Document {
   agencyName: string;
-  address: string; 
+  address: string;
+  ownerName: string;
+  ownerLastName: string;
+
+  expirationDate: string;
+  numberTva: string;
+  zipCode: string;
+  city: string;
+  region: string;
+  country: string;
+  website: string;
+  bankName: string;
+  rib: string;
+  iban: string;
+  logo: string; 
   cars: mongoose.Types.ObjectId[];
   idUser: mongoose.Types.ObjectId;
 }
 
-const agencySchema = new mongoose.Schema({
-  agencyName: { type: String, required: true , unique: true },
+const agencySchema = new mongoose.Schema<IAgency>({
+  agencyName: { type: String, required: true },
   address: { type: String, required: true },
+  ownerName: { type: String, required: true },
+  ownerLastName: { type: String, required: true },
+  expirationDate: { type: String, required: true },
+  numberTva: { type: String, required: true },
+  zipCode: { type: String, required: true },
+  city: { type: String, required: true },
+  region: { type: String, required: true },
+  country: { type: String, required: true },
+  website: { type: String, required: true },
+  bankName: { type: String, required: true },
+  rib: { type: String, required: true },
+  iban: { type: String, required: true },
+  logo: { type: String, required: true },
+  idUser: { type: mongoose.Schema.Types.ObjectId, ref: 'user' },
+  cars: [{ type: mongoose.Schema.Types.ObjectId, ref: 'Cars' }],
+}, { timestamps: true });
 
-  createdAt: {
-    type: Date,
-    default: new Date(),
-  },
-  idUser: {
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'user', 
-  },
-  cars: [{
-    type: mongoose.Schema.Types.ObjectId,
-    ref: 'Cars', 
-  }],
-});
+const AgencyModel = mongoose.model<IAgency>("Agency", agencySchema);
 
-const agencys = mongoose.model<IAgency>("agencys", agencySchema);
-export default agencys;
+export default AgencyModel;
