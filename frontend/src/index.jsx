639ad8a4b468819fa7696b22fe3f6f33bc4fee30
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-18 22:59:44 +0000

    Add Redux store provider to index.jsx

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -11,14 +11,20 @@ import "slick-carousel/slick/slick-theme.css";
 import { BrowserRouter as Router } from "react-router-dom";
 import { ChakraProvider } from "@chakra-ui/react";
 
+import store from "./store";
+import { Provider } from "react-redux";
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
+
+    <Provider store={store}>
     <ChakraProvider>
     <Router>
       <App />
     </Router>
     </ChakraProvider>
+    </Provider>
 
   </React.StrictMode>
 );
