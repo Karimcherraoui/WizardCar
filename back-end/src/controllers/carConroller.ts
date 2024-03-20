a920a1afb6e7c36659b2fcd557c3bfc99f8fb95c
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-20 22:02:24 +0000

    Refactor carController.ts and add new features

--- back-end/src/controllers/carConroller.ts
+++ back-end/src/controllers/carConroller.ts
@@ -1,9 +1,18 @@
 import { Request, Response, json } from "express";
-import Car , {ICar} from "../models/Cars";
+import Car, { ICar } from "../models/Cars";
 import Agency, { IAgency } from "../models/Agency";
 
 import z from "zod";
-import Cars from "../models/Cars";
+
+import userModel from "../models/User";
+
+interface AuthenticatedRequest extends Request {
+  user?: {
+    userId: string;
+    // Add any other properties related to the user if needed
+  };
+}
+  
 
 const registrationSchema = z.object({
   brand: z.string(),
@@ -15,7 +24,9 @@ const registrationSchema = z.object({
   type: z.string(),
   transmission: z.string(),
   plateNumber: z.string(),
-  idAgency: z.string(),
+  description: z.string(),
+  chassisNumber: z.string(),
+  image: z.string(),
 });
 
 export const carController = {
@@ -32,16 +43,17 @@ export const carController = {
     const { id } = req.params;
     try {
       const car = await Car.findById(id);
-      res.status(200).json({ car});
+      res.status(200).json({ car });
     } catch (error) {
       console.error(error);
-    } 
+    }
   },
 
   createCar: async (req: Request, res: Response) => {
     try {
       const data = registrationSchema.parse(req.body);
-
+     const idUser = (req as AuthenticatedRequest).user?.userId;
+     
       const {
         brand,
         model,
@@ -52,61 +64,75 @@ export const carController = {
         type,
         transmission,
         plateNumber,
-        idAgency
+        chassisNumber,
+        description,
+        image,
       } = data;
 
-      const agency = await Agency.findById(idAgency);
+      const user = await userModel.findById(idUser);
+      const idAgence = user?.referredUser?.toString();
+      const agency = await Agency.findById(idAgence);
       if (!agency) {
         return res.status(404).json({ error: "Agency not found" });
       }
 
-     const newCar: ICar = await Car.create({
-      brand,
-      model,
-      year,
-      color,
-      price,
-      fuel,
-      type,
-      transmission,
-      plateNumber,
-      idAgency
-    });
+      const newCar: ICar = await Car.create({
+        brand,
+        model,
+        year,
+        color,
+        price,
+        fuel,
+        type,
+        transmission,
+        plateNumber,
+        chassisNumber,
+        description,
+        image,
+        idAgence,
+      });
 
-  const agencyCar = await Agency.findByIdAndUpdate(idAgency, { $push: { cars: newCar._id } }, { new: true });
-
-    if (!agencyCar) {
-      return res.status(404).json("Agency not found");
-    }
+       await Agency.findByIdAndUpdate(
+        idAgence,
+        { $push: { cars: newCar._id } },
+        { new: true }
+      );
       res.status(201).json({
         message: "Car created successfully",
         car: newCar,
       });
     } catch (error) {
-      if ((error as { name: string; code: number }).name === 'MongoServerError' && (error as { name: string; code: number }).code === 11000) {
-        return res.status(400).json({ error: "A Car with the same information already exists." });
+      if (
+        (error as { name: string; code: number }).name === "MongoServerError" &&
+        (error as { name: string; code: number }).code === 11000
+      ) {
+        return res
+          .status(400)
+          .json({ error: "A Car with the same information already exists." });
       }
       if (error instanceof z.ZodError) {
         return res.status(400).json({ error: error.errors[0].message });
       }
       console.error(error);
-
     }
   },
 
-  returnCar: async (req:Request, res:Response)=>{
-    const {id} = req.params
-    try{
-      const car = await Car.findByIdAndUpdate(id,{disponibility: "Disponible"},{new:true})
+  returnCar: async (req: Request, res: Response) => {
+    const { id } = req.params;
+    try {
+      const car = await Car.findByIdAndUpdate(
+        id,
+        { disponibility: "Disponible" },
+        { new: true }
+      );
       if (!car) {
         return res.status(404).json({ error: "Car not found" });
       }
       res.status(200).json({
         message: "Car Disponible",
       });
-
-    }catch(error){
-      console.error(error)
+    } catch (error) {
+      console.error(error);
     }
   },
 
@@ -131,7 +157,7 @@ export const carController = {
   deleteCar: async (req: Request, res: Response) => {
     const { id } = req.params;
     try {
-      const car =  await Car.findByIdAndDelete(id);
+      const car = await Car.findByIdAndDelete(id);
       if (!car) {
         return res.status(404).json({ error: "Car not found" });
       }
