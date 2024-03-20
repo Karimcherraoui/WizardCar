32f70d5753ccc1eab7f18565ca7a11ca1d665a04
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-20 22:02:30 +0000

    Add uploadController to handle file uploads

---
+++ back-end/src/controllers/uploadController.ts
@@ -0,0 +1,19 @@
+import { Request, Response } from "express";
+
+export const uploadController = {
+  upload: async (req: Request, res: Response) => {
+    try {
+      const file = req.file;
+
+      if (!file) {
+        return res.status(400).send("Please upload a file");
+      }
+      res.json({
+        message: "File uploaded successfully",
+        url: `${req.protocol}://${req.get("host")}/${file.filename}`,
+      });
+    } catch (error) {
+      res.status(500).send(error);
+    }
+  },
+};
