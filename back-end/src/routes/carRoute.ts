4fb0607a0f019d3d66c3ee74aba426979374a3c4
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-20 22:02:59 +0000

    Add upload middleware to carRoute

--- back-end/src/routes/carRoute.ts
+++ back-end/src/routes/carRoute.ts
@@ -2,13 +2,14 @@ import { Router } from "express";
 import { carController } from "../controllers/carConroller";
 import { authenticateToken } from "../middleware/jwt";
 import { checkRole } from "../middleware/checkRole";
+import upload from "../middleware/upload";
 
 const routerCar = Router();
 
 routerCar.get("/", carController.getAllCars);
 routerCar.get("/:id", carController.getCar);
 routerCar.use(authenticateToken);
-routerCar.patch("/:id", checkRole.agency, carController.updateCar);
+routerCar.patch("/:id", checkRole.agency ,carController.updateCar);
 routerCar.patch("/returnCar/:id", checkRole.agency, carController.returnCar);
 routerCar.delete("/:id", checkRole.agency, carController.deleteCar);
 routerCar.post("/", checkRole.agency, carController.createCar);
