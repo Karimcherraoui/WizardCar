902cdb6d8bb4970480834bffc08da9b811e2c381
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-22 23:01:45 +0000

    Add PrivateRouteClient component

---
+++ frontend/src/routers/PrivateRouteClient.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import { Navigate } from 'react-router-dom'
+const PrivateRouteClient= ({children}) => {
+  const user = localStorage.getItem("User");
+    if (!user) {
+        return <Navigate to="/login" />;
+    }
+  return children
+
+}
+
+export default PrivateRouteClient
