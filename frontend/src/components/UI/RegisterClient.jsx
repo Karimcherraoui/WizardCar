cde0741645c493ca4a5637f9e29ed29f2b327967
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-25 09:43:20 +0000

    Refactor RegisterClient component

--- frontend/src/components/UI/RegisterClient.jsx
+++ frontend/src/components/UI/RegisterClient.jsx
@@ -15,8 +15,8 @@ export default function RegisterClient() {
 
   const handleSubmit = (e) => {
     e.preventDefault();
-    const form = { ...localSettings }; 
-  dispatch(signupClient({ form }));
+    const form = { ...localSettings };
+    dispatch(signupClient({ form }));
   };
 
   return (
@@ -29,8 +29,9 @@ export default function RegisterClient() {
           </div>
           <div className="divide-y divide-gray-200">
             <form
-            onSubmit={handleSubmit}
-             className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7">
+              onSubmit={handleSubmit}
+              className="py-8 text-base leading-6 space-y-4 text-gray-700 sm:text-lg sm:leading-7"
+            >
               <div className="flex gap-20">
                 <div className="relative">
                   <input
@@ -291,9 +292,32 @@ export default function RegisterClient() {
                   </label>
                 </div>
               </div>
+              <div className="flex gap-20">
+                <div className="relative ">
+                  <input
+                    onChange={handleChange}
+                    autocomplete="off"
+                    id="zip"
+                    name="zipCode"
+                    type="text"
+                    className="peer placeholder-transparent h-10 w-full border-b-2 border-gray-300 text-gray-900 focus:outline-none focus:borer-rose-600"
+                    placeholder="Zip Code"
+                  />
+                  <label
+                    htmlFor="zipCode"
+                    className="absolute left-0 -top-3.5 text-gray-600 text-sm peer-placeholder-shown:text-base peer-placeholder-shown:text-gray-440 peer-placeholder-shown:top-2 transition-all peer-focus:-top-3.5 peer-focus:text-gray-600 peer-focus:text-sm"
+                  >
+                    Zip Code
+                  </label>
+                </div>
+                
+              </div>
 
               <div className="relative text-center  ">
-                <button type="submit" className="bg-green-500 text-white rounded-md px-4 py-2 mt-4">
+                <button
+                  type="submit"
+                  className="bg-green-500 text-white rounded-md px-4 py-2 mt-4"
+                >
                   Create Account
                 </button>
               </div>
