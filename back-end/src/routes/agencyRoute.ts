6b56aeba53d5466e9589fafa69d20ac1bc28cca2
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-25 09:42:26 +0000

    Refactor agencyRoute.ts

--- back-end/src/routes/agencyRoute.ts
+++ back-end/src/routes/agencyRoute.ts
@@ -2,23 +2,14 @@ import { Router } from "express";
 import { agencyController } from "../controllers/agencyController";
 import { authenticateToken } from "../middleware/jwt";
 import { checkRole } from "../middleware/checkRole";
-import router from "routes";
 
 const routerAgency = Router();
-routerAgency.get("/profile", authenticateToken,agencyController.getProfile);
+routerAgency.get("/profile", authenticateToken, agencyController.getProfile);
 
 routerAgency.get("/", agencyController.getAllAgencys);
 routerAgency.get("/:id", agencyController.getAgency);
 routerAgency.use(authenticateToken);
-routerAgency.patch(
-  "/:id",
-  checkRole.agency,
-  agencyController.updateAgency
-);
-routerAgency.delete(
-  "/:id",
-  checkRole.agency,
-  agencyController.deleteAgency
-);
+routerAgency.patch("/", checkRole.agency, agencyController.updateAgency);
+routerAgency.delete("/:id", checkRole.agency, agencyController.deleteAgency);
 
 export default routerAgency;
