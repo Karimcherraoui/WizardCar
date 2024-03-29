becc7c1b6a8273585077fa6025bf39e36c0ac1bb
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-29 10:33:30 +0000

    Refactor TableFacture component and fetch invoices

--- frontend/src/components/Tables/TableFacture.jsx
+++ frontend/src/components/Tables/TableFacture.jsx
@@ -9,115 +9,102 @@ import {
   Thead,
   Tr,
 } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { useDispatch, useSelector } from "react-redux";
+import { fetchInvoices } from "../../features/invoicesSlice";
 
-export const    TableFacture = () => {
-
+export const TableFacture = () => {
   const dispatch = useDispatch();
   const profile = useSelector((state) => state.agency.profile);
-
-
-  console.log("profile", profile?.agency.cars);
-
-
-  
-  
-
+  const reservations = useSelector((state) => state.invoice.invoices);
+  const agenceId = profile?.agency._id;
+  useEffect(() => {
+    dispatch(fetchInvoices(agenceId));
+  }, [dispatch]);
+console.log(reservations);
   return (
-    <div class="relative overflow-x-auto sm:rounded-lg   rounded-lg shadow-lg my-10 mx-24">
-      <table class="w-full text-sm text-left rtl:text-right text-gray-500">
-        <thead class="text-xs text-gray-700 uppercase bg-gray-50 ">
+    <div className="relative overflow-x-auto sm:rounded-lg   rounded-lg shadow-lg my-10 mx-24">
+      <table className="w-full text-sm text-left rtl:text-right text-gray-500">
+        <thead className="text-xs text-gray-700 uppercase bg-gray-50 ">
           <tr>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Product name
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Color
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Category
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Price
             </th>
-            <th scope="col" class="px-6 py-3">
+            <th scope="col" className="px-6 py-3">
               Action
             </th>
           </tr>
         </thead>
         <tbody>
-          <tr class="odd:bg-white  even:bg-gray-50 border-b ">
+          <tr className="odd:bg-white  even:bg-gray-50 border-b ">
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Apple MacBook Pro 17"
             </th>
-            <td class="px-6 py-4">Silver</td>
-            <td class="px-6 py-4">Laptop</td>
-            <td class="px-6 py-4">$2999</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600  hover:underline"
-              >
+            <td className="px-6 py-4">Silver</td>
+            <td className="px-6 py-4">Laptop</td>
+            <td className="px-6 py-4">$2999</td>
+            <td className="px-6 py-4">
+              <a href="#" className="font-medium text-blue-600  hover:underline">
                 Edit
               </a>
             </td>
           </tr>
-          <tr class="odd:bg-white  even:bg-gray-50  border-b ">
+          <tr className="odd:bg-white  even:bg-gray-50  border-b ">
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Microsoft Surface Pro
             </th>
-            <td class="px-6 py-4">White</td>
-            <td class="px-6 py-4">Laptop PC</td>
-            <td class="px-6 py-4">$1999</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600  hover:underline"
-              >
+            <td className="px-6 py-4">White</td>
+            <td className="px-6 py-4">Laptop PC</td>
+            <td className="px-6 py-4">$1999</td>
+            <td className="px-6 py-4">
+              <a href="#" className="font-medium text-blue-600  hover:underline">
                 Edit
               </a>
             </td>
           </tr>
-          <tr class="odd:bg-white  even:bg-gray-50  border-b dar">
+          <tr className="odd:bg-white  even:bg-gray-50  border-b dar">
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Magic Mouse 2
             </th>
-            <td class="px-6 py-4">Black</td>
-            <td class="px-6 py-4">Accessories</td>
-            <td class="px-6 py-4">$99</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600 hover:underline"
-              >
+            <td className="px-6 py-4">Black</td>
+            <td className="px-6 py-4">Accessories</td>
+            <td className="px-6 py-4">$99</td>
+            <td className="px-6 py-4">
+              <a href="#" className="font-medium text-blue-600 hover:underline">
                 Edit
               </a>
             </td>
           </tr>
-          <tr class="odd:bg-white  even:bg-gray-50  border-b ">
+          <tr className="odd:bg-white  even:bg-gray-50  border-b ">
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Google Pixel Phone
             </th>
-            <td class="px-6 py-4">Gray</td>
-            <td class="px-6 py-4">Phone</td>
-            <td class="px-6 py-4">$799</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600  hover:underline"
-              >
+            <td className="px-6 py-4">Gray</td>
+            <td className="px-6 py-4">Phone</td>
+            <td className="px-6 py-4">$799</td>
+            <td className="px-6 py-4">
+              <a href="#" className="font-medium text-blue-600  hover:underline">
                 Edit
               </a>
             </td>
@@ -125,18 +112,15 @@ export const    TableFacture = () => {
           <tr>
             <th
               scope="row"
-              class="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
+              className="px-6 py-4 font-medium text-gray-900 whitespace-nowrap "
             >
               Apple Watch 5
             </th>
-            <td class="px-6 py-4">Red</td>
-            <td class="px-6 py-4">Wearables</td>
-            <td class="px-6 py-4">$999</td>
-            <td class="px-6 py-4">
-              <a
-                href="#"
-                class="font-medium text-blue-600  hover:underline"
-              >
+            <td className="px-6 py-4">Red</td>
+            <td className="px-6 py-4">Wearables</td>
+            <td className="px-6 py-4">$999</td>
+            <td className="px-6 py-4">
+              <a href="#" className="font-medium text-blue-600  hover:underline">
                 Edit
               </a>
             </td>
