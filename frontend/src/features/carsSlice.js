f6b23d904ec8982eeab336583b961e19a8fa29c0
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-29 10:33:50 +0000

    Refactor carSlice.js to remove unnecessary code

--- frontend/src/features/carsSlice.js
+++ frontend/src/features/carsSlice.js
@@ -12,15 +12,9 @@ export const fetchCars = createAsyncThunk(
   "cars/fetchCars",
   async (_, thunkAPI) => {
     try {
-      const response = await axios.get(`http://localhost:3005/car/`, {
-        headers: {
-          Authorization: `Bearer ${
-            JSON.parse(localStorage.getItem("User")).tokenKey
-          }`,
-        },
-      });
+      const response = await axios.get(`http://localhost:3005/car/`);
       if (response.status >= 200 && response.status <= 299) {
-        console.log(response.data);
+
         return response.data;
       } else {
         throw new Error("Failed to fetch cars");
@@ -35,13 +29,7 @@ export const fetchCar = createAsyncThunk(
   "cars/fetchCar",
   async ( id , thunkAPI) => {
     try {
-      const response = await axios.get(`http://localhost:3005/car/${id}`, {
-        headers: {
-          Authorization: `Bearer ${
-            JSON.parse(localStorage.getItem("User")).tokenKey
-          }`,
-        },
-      });
+      const response = await axios.get(`http://localhost:3005/car/${id}`);
       if (response.status >= 200 && response.status <= 299) {
         return response.data;
       }
@@ -55,7 +43,7 @@ export const addCar = createAsyncThunk(
   "cars/addCar",
   async ({ form }, thunkAPI) => {
     try {
-      console.log(form);
+
       const response = await axios.post(`http://localhost:3005/car/`, form, {
         headers: {
           Authorization: `Bearer ${
