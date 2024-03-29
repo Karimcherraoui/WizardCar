e5928fa6fad5a23e8ea6b8ce2b3178bb84167641
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-29 10:32:43 +0000

    Refactor reservationController.ts

--- back-end/src/controllers/reservationController.ts
+++ back-end/src/controllers/reservationController.ts
@@ -1,7 +1,6 @@
 import { Request, Response, json } from "express";
 import Reservation, { IReservation } from "../models/Reservation";
 
-
 import z from "zod";
 import clients from "../models/Client";
 import Cars from "../models/Cars";
@@ -10,35 +9,50 @@ const registrationSchema = z.object({
   pickupDate: z.string(),
   returnDate: z.string(),
   pickUpType: z.string(),
-  pickUpLocation: z.string(),
+  pickUpLocation: z.string().optional(), 
   paymentMethod: z.string(),
   message: z.string(),
-  status: z.string(),
+  status: z.string().optional(), 
   idClient: z.string(),
   idCar: z.string(),
+  idAgence: z.string(),
 });
-const updateResSchema = registrationSchema.partial().omit({ idClient: true, idCar: true,pickupDate:true });
+const updateResSchema = registrationSchema
+  .partial()
+  .omit({ idClient: true, idCar: true, pickupDate: true });
 
-const calculateTotalPrice = (pickupDate: string, returnDate: string, priceUnitaire: number): number => {
-  const [pickupDay, pickupMonth, pickupYear] = pickupDate.split("-").map(Number);
-  const [returnDay, returnMonth, returnYear] = returnDate.split("-").map(Number);
-  const pickupDateCar = new Date(pickupYear, pickupMonth - 1, pickupDay);
-  const returnDateCar = new Date(returnYear, returnMonth - 1, returnDay);
-  const differenceInTime = returnDateCar.getTime() - pickupDateCar.getTime();
-  const differenceInDays = differenceInTime / (1000 * 3600 * 24);
+const calculateTotalPrice = (
+  pickupDate: string,
+  returnDate: string,
+  priceUnitaire: number
+): number => {
+  const [pickupYear, pickupMonth, pickupDay] = pickupDate
+  .split("-")
+  .map(Number);
+const [returnYear, returnMonth, returnDay] = returnDate
+  .split("-")
+  .map(Number);
+
+const pickupDateCar = new Date(pickupYear, pickupMonth - 1, pickupDay);
+const returnDateCar = new Date(returnYear, returnMonth - 1, returnDay);
+
+const differenceInTime = returnDateCar.getTime() - pickupDateCar.getTime();
+const differenceInDays = differenceInTime / (1000 * 3600 * 24);
   return differenceInDays * priceUnitaire;
 };
-
-
-
 
 export const reservationController = {
   getAllReservations: async (req: Request, res: Response) => {
     try {
-      const reservations = await Reservation.find();
+
+      const idAgence = req.params.id
+      const reservations = await Reservation.find({idAgence});
+
+      
       res.status(200).json({ reservations });
     } catch (error) {
       console.error(error);
+      res.status(500).json({ error: 'Internal server error' });
     }
   },
 
@@ -52,50 +66,63 @@ export const reservationController = {
     }
   },
 
+
   createReservation: async (req: Request, res: Response) => {
     try {
       const data = registrationSchema.parse(req.body);
+
+
       const {
         pickupDate,
+        pickUpType,
+        paymentMethod,
         returnDate,
-        status, 
+        message,
         idClient,
         idCar,
+        idAgence,
       } = data;
 
-      const car = await Cars.findById(idCar)
-      if(!car){
-        return  res.status(404).json({error:"Car not Found"})
+      const car = await Cars.findById(idCar);
+      if (!car) {
+        return res.status(404).json({ error: "Car not Found" });
       }
 
       const priceUnitaire = car.price;
-      const totalPriceCar = calculateTotalPrice(pickupDate, returnDate, +priceUnitaire);
+      const totalPriceCar = calculateTotalPrice(
+        pickupDate,
+        returnDate,
+        +priceUnitaire
+      ).toFixed(2);
 
-const newResrvation: IReservation = new Reservation({
-  pickupDate,
-  returnDate,
-  price: priceUnitaire,
-  totalPrice: totalPriceCar,
-  status,
-  idClient,
-  idCar,
-});
+      const newResrvation: IReservation = new Reservation({
+        pickupDate,
+        returnDate,
+        message,
+        paymentMethod,
+        price: priceUnitaire,
+        totalPrice: totalPriceCar,
+        pickUpType,
+        idClient,
+        idCar,
+        idAgence,
+      });
 
-const client = await clients.findById(idClient);
+      const client = await clients.findById(idClient);
 
-      if(!client){
-        return  console.log("Client Not Found");
+      if (!client) {
+        return console.log("Client Not Found");
       }
-      if(!car){
-        return  res.status(404).json({error:"Car not Found"})
-      }else{
-        if(car.disponibility === "Non disponible"){
-          return  res.status(406).json({error:"The Car Already Reserved"})
+      if (!car) {
+        return res.status(404).json({ error: "Car not Found" });
+      } else {
+        if (car.disponibility === "Non disponible") {
+          return res.status(406).json({ error: "The Car Already Reserved" });
         }
-        car.disponibility =  "Non disponible"
+        car.disponibility = "Non disponible";
         await car.save();
       }
-      
+
       const savedReservation = await newResrvation.save();
 
       res.status(201).json({
@@ -103,6 +130,8 @@ const client = await clients.findById(idClient);
         reservation: savedReservation,
       });
     } catch (error) {
+      console.log(error);
+      
       if (error instanceof z.ZodError) {
         return res.status(400).json({ error: error.errors[0].message });
       }
@@ -125,11 +154,19 @@ const client = await clients.findById(idClient);
         return res.status(404).json({ error: "Car not found" });
       }
       const priceUnitaire = car.price;
-      const totalPriceCar = calculateTotalPrice(reservation.pickupDate, reservation.returnDate, +priceUnitaire);
-      const updatedReservation = await Reservation.findByIdAndUpdate(id, {
-        ...data,
-        totalPrice: totalPriceCar,
-      }, { new: true });
+      const totalPriceCar = calculateTotalPrice(
+        reservation.pickupDate,
+        reservation.returnDate,
+        +priceUnitaire
+      );
+      const updatedReservation = await Reservation.findByIdAndUpdate(
+        id,
+        {
+          ...data,
+          totalPrice: totalPriceCar,
+        },
+        { new: true }
+      );
 
       res.status(200).json({
         message: "Reservation updated successfully",
