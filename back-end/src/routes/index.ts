dfc69b0fea721daf7997c54bb84bcdc066e409eb
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-29 10:32:58 +0000

    Refactor routes and add invoice route

--- back-end/src/routes/index.ts
+++ back-end/src/routes/index.ts
@@ -1,32 +1,29 @@
-import { Router } from "express"
-import carRoute from "./carRoute"
-import agencyRoute from "./agencyRoute"
-import clientRoute from "./clientRoute"
-import reservationRoute from "./reservationRoute"
-import authRoute from "./authRoute"
+import { Router } from "express";
+import carRoute from "./carRoute";
+import agencyRoute from "./agencyRoute";
+import clientRoute from "./clientRoute";
+import reservationRoute from "./reservationRoute";
+import authRoute from "./authRoute";
 
+import { json } from "express";
+import upload from "../middleware/upload";
+import { uploadController } from "../controllers/uploadController";
+import uploadLogo from "../middleware/uploadLogo";
+import { uploadLogoController } from "../controllers/uploadLogoController";
 
+const router = Router();
+router.use(json());
+router.post(
+  "/uploadLogo",
+  uploadLogo.single("logo"),
+  uploadLogoController.upload
+);
+router.post("/upload", upload.single("image"), uploadController.upload);
+router.use("/auth", authRoute);
+router.use("/car", carRoute);
+router.use("/invoice", reservationRoute);
+router.use("/agency", agencyRoute);
+router.use("/client", clientRoute);
+router.use("/reservation", reservationRoute);
 
-import { json } from "express"; 
-import upload from "../middleware/upload"
-import { uploadController } from "../controllers/uploadController"
-import uploadLogo from "../middleware/uploadLogo"
-import { uploadLogoController } from "../controllers/uploadLogoController"
-
-
-
-
-const router  =  Router();
-router.use(json())
-router.post("/uploadLogo", uploadLogo.single("logo"), uploadLogoController.upload)
-router.post("/upload", upload.single("image"), uploadController.upload)
-router.use("/auth" ,authRoute )
-router.use("/car" , carRoute)
-router.use("/agency" , agencyRoute)
-router.use("/client" , clientRoute)
-router.use("/reservation" , reservationRoute)
-
-
-
-
-export default router
+export default router;
