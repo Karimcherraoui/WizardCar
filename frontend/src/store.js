a58f736ac6a4808d34545a4f409ccc434f2cc511
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-03-29 10:33:13 +0000

    Add invoiceReducer to store.js

--- frontend/src/store.js
+++ frontend/src/store.js
@@ -3,6 +3,7 @@ import agencyReducer from "./features/agencySlice";
 import clientReducer from "./features/clientSlice";
 import userReducer from "./features/userSlice";
 import carReducer from "./features/carsSlice";
+import invoiceReducer from "./features/invoicesSlice";
 
 
 
@@ -11,6 +12,7 @@ const store = configureStore({
     client: clientReducer,
     agency: agencyReducer ,
     user: userReducer,
+    invoice: invoiceReducer,
     car: carReducer,
   },
 });
