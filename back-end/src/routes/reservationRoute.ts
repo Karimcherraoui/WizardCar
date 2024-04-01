82ba421fa095a0aaf9c5e3b7507cdebc83880929
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-04-01 15:23:29 +0000

    Add client reservation route

--- back-end/src/routes/reservationRoute.ts
+++ back-end/src/routes/reservationRoute.ts
@@ -9,6 +9,7 @@ const routerReservation = Router();
 // routerReservation.use(checkRole.agency);
 routerReservation.post("/", reservationController.createReservation);
 routerReservation.get("/list/:id", reservationController.getAllReservations);
+routerReservation.get("/client-reservation/:id", reservationController.getAllReservationsClient);
 routerReservation.get("/:id", reservationController.getReservation);
 routerReservation.patch("/:id", reservationController.updateReservation);
 routerReservation.delete("/:id", reservationController.deleteReservation);
