4383e66f80056e247b10e1c52b9ff7951943a530
Karimcherraoui <cherraoui.karim96@gmail.com>
2024-04-01 15:24:37 +0000

    Update key attribute in CarItem component

--- frontend/src/pages/CarListing.jsx
+++ frontend/src/pages/CarListing.jsx
@@ -26,7 +26,7 @@ const CarListing = () => {
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 xl:grid-cols-4 gap-4 mb-20">
           {cars?.map((item) => (
-            <CarItem item={item} key={item.id} />
+            <CarItem item={item} key={item._id} />
           ))}
         </div>
       </section>
